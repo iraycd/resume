d81f816d45a533261fc21c01addba5a15c417831
Ray <i@iraycd.com>
2019-02-12 18:07:06 +0530

    Updates

---
+++ precache-manifest.8f10055a2cef1afd037d60dc48376a98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1227c6a2f59d072c3a6216badbee1411",
+    "url": "/resume/static/media/RayChintalapatiResume.1227c6a2.jpg"
+  },
+  {
+    "revision": "71aca9b7f34bbde7ad91917c4f741568",
+    "url": "/resume/static/media/Award.71aca9b7.jpg"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.fd78f13f.js"
+  },
+  {
+    "revision": "07ad02fba3d0923f8a2d",
+    "url": "/resume/static/js/main.07ad02fb.chunk.js"
+  },
+  {
+    "revision": "e6f01a8371da550a167e",
+    "url": "/resume/static/js/2.e6f01a83.chunk.js"
+  },
+  {
+    "revision": "07ad02fba3d0923f8a2d",
+    "url": "/resume/static/css/main.1dd2d1fe.chunk.css"
+  },
+  {
+    "revision": "13fc0d6a43482b26d0aeb98a16652967",
+    "url": "/resume/index.html"
+  }
+];
